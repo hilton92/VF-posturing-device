1716076409a15d8ab75fa9cb9b71c1c28df3907e
Benjamin Hilton <benjamin.hilton92@gmail.com>
2019-06-06 21:42:36 -0600

    added stepper code in main

--- main.py
+++ main.py
@@ -7,9 +7,9 @@
 
 import numpy as np
 import math
-from gpiozero import Button # included with raspbian
-import board # included with raspbian
-import digitalio #from adafruit-blinka library, to install run "$ sudo pip3 install adafruit-blinka"
+import RPi.GPIO as GPIO
+import Stepper
+
 
 # multiplication by this rotation matrix transforms the right side origin into the base origin
 T_d = np.array([[0.9063,-0.4226,26],[0.4226,0.9063,20],[0,0,1]])
@@ -20,13 +20,65 @@ T_c = np.array([[0.9063,0.4226,-69.5028],[-0.4226,0.9063,40.2857],[0,0,1]])
 # multiplication by this rotation matrix transforms the base origin to the left side origin
 T_cINV = np.array([[0.9063,0.4226,-69.5028],[-0.4226,0.9063,40.2857],[0,0,1]])
 
+GPIO.setmode(GPIO.BOARD)
+
 # Declare stepper motors
+StepperA_PUL = 47
+StepperA_DIR = 49
+StepperA_ENA = 51
+StepperB_PUL = 46
+StepperB_DIR = 48
+StepperB_ENA = 50
+StepperC_PUL = 39
+StepperC_DIR = 41
+StepperC_ENA = 43
+StepperD_PUL = 38
+StepperD_DIR = 40
+StepperD_ENA = 42
+
+LimitA_signal = 29
+LimitB_signal = 27
+LimitC_signal = 25
+LimitD_signal = 23
+
+GPIO.setup(StepperA_PUL, GPIO.OUT)
+GPIO.setup(StepperA_DIR, GPIO.OUT)
+GPIO.setup(StepperA_ENA, GPIO.OUT)
+GPIO.setup(StepperB_PUL, GPIO.OUT)
+GPIO.setup(StepperB_DIR, GPIO.OUT)
+GPIO.setup(StepperB_ENA, GPIO.OUT)
+GPIO.setup(StepperC_PUL, GPIO.OUT)
+GPIO.setup(StepperC_DIR, GPIO.OUT)
+GPIO.setup(StepperC_ENA, GPIO.OUT)
+GPIO.setup(StepperD_PUL, GPIO.OUT)
+GPIO.setup(StepperD_DIR, GPIO.OUT)
+GPIO.setup(StepperD_ENA, GPIO.OUT)
+
+GPIO.setup(LimitA_signal, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(LimitB_signal, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(LimitC_signal, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(LimitD_signal, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 
+
+StepperA  = Stepper(StepperA_PUL, StepperA_DIR, StepperA_ENA, 488, LimitA_signal)
+StepperB  = Stepper(StepperB_PUL, StepperB_DIR, StepperB_ENA, 336, LimitB_signal)
+StepperC  = Stepper(StepperC_PUL, StepperC_DIR, StepperC_ENA, 512, LimitC_signal)
+StepperD  = Stepper(StepperD_PUL, StepperD_DIR, StepperD_ENA, 527, LimitD_signal)
+
+# Zero the Stepper Motors
+
+StepperA.zero_stepper()
+StepperB.zero_stepper()
+StepperC.zero_stepper()
+StepperD.zero_stepper()
 
 
 while True:
-    # zero stepper motors
+     val = input("Enter X and Y in the form X_Y (no spaces)")
+     X, Y = val.split("_",1)
+    theta1, theta2 = transform(X,Y, )
+
 
 
 
